ebb021afe1e569e23138886446763ab40b817297
Ayo <abdulmohammed116@rams.wssu.edu>
2021-11-09 17:08:02 -0500

    Additional Budget functinoallity

--- app/src/main/java/com/example/budgetingapp/MainActivity.java
+++ app/src/main/java/com/example/budgetingapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.budgetingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView budgetCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        budgetCardView = findViewById(R.id.budgetCardview);
+
+        budgetCardView.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,BudgetActivity.class);
+            startActivity(intent);
+         });
+
     }
 }
